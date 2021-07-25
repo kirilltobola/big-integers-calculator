74419cc8c0d11fed071feaa360d2b746367ae21b
Kirill Tobola <kirilltobola@gmail.com>
2021-07-25 13:48:30 +0800

    Validate input data

--- handlers/index.go
+++ handlers/index.go
@@ -3,10 +3,17 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+	"regexp"
+)
+
+const (
+	NUMBERS = iota
+	POLYNOMIALS
 )
 
 type Data struct {
-	Result string
+	Expression string
+	Result     string
 }
 
 func IndexGetHandler(writer http.ResponseWriter, request *http.Request) {
@@ -17,9 +24,31 @@ func IndexGetHandler(writer http.ResponseWriter, request *http.Request) {
 func IndexPostHandler(writer http.ResponseWriter, request *http.Request) {
 	template := template.Must(template.ParseFiles("html/index.html"))
 	request.ParseForm()
+	inputExpression := request.FormValue("expression")
+	switch validateInput(inputExpression) {
+	case NUMBERS:
+		return
+	case POLYNOMIALS:
+		return
+	}
 
 	data := Data{
-		Result: request.FormValue("expression"),
+		Expression: request.FormValue("expression"),
+		Result:     "",
 	}
 	template.Execute(writer, data)
+}
+
+func validateInput(input string) int {
+	numbersPattern := `^\d+\*\d+$`
+	polysPattern := `^\(\d+(\s\d+)*\)\*\(\d+(\s\d+)*\)$`
+	polynomials, _ := regexp.Match(polysPattern, []byte(input))
+	numbers, _ := regexp.Match(numbersPattern, []byte(input))
+
+	if polynomials {
+		return POLYNOMIALS
+	} else if numbers {
+		return NUMBERS
+	}
+	panic("Incorrect input!")
 }
