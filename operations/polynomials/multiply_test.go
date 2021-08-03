ef38caffdd3440603cb61983bd21b1e55eb523b7
Kirill Tobola <kirilltobola@gmail.com>
2021-08-03 23:18:05 +0800

    Add multiply polynomials test

---
+++ operations/polynomials/multiply_test.go
@@ -0,0 +1,18 @@
+package polynomials_test
+
+import (
+	"big-integers-calculator/operations/polynomials"
+	"big-integers-calculator/types"
+	"testing"
+)
+
+func TestMultiply(t *testing.T) {
+	poly1 := []complex128{3, 2, 1, 0, 0, 0, 0, 0}
+	poly2 := []complex128{3, 2, 1, 0, 0, 0, 0, 0}
+	got := polynomials.Multiply(poly1, poly2)
+	expected := types.Poly{9, 12, 10, 4, 1, 0, 0, 0}
+
+	if !got.Equal(&expected) {
+		t.Errorf("got %v expected %v", got, expected)
+	}
+}
